fbd5988d372dc3175ba2b9d8236cec28ecc9bcc4
Daniil Jasper <jasper@deeplay.io>
2025-08-10 14:37:01 +0600

    fix: правки

--- store/configuratorStore.ts
+++ store/configuratorStore.ts
@@ -220,7 +220,7 @@ export const useConfiguratorStore = defineStore('configuratorStore', {
             const popupWindow = window.open()
             // Здесь вызовите ваш API, который вернёт ссылку на оплату через СБП
             // Например:
-            fetch('/api/spb-create', {
+            fetch('/spb-create', {
                 method: 'POST',
                 body: JSON.stringify(orderData),
                 headers: { 'Content-Type': 'application/json' }
